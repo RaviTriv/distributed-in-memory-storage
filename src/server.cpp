9d91fa7cd0dc761d022b0129d8e25a7d559fd9a6
Ravi <trivediravi561@gmail.com>
2024-08-28 21:06:47 -0400

    rough implementation of handling multiple clients

--- src/server.cpp
+++ src/server.cpp
@@ -9,50 +9,90 @@
 #include <unistd.h>
 #include <string.h>
 #include <netdb.h>
+#include <pthread.h>
 
 using namespace std;
-
-int main(int argc, char *argv[])
+int port = 2401;
+char message[2100];
+sockaddr_in serverAddress;
+int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+sockaddr_in clientAddress;
+socklen_t clientAddressLength = sizeof(clientAddress);
+int clientDescriptor1;
+int clientDescriptor2;
+pthread_t t1;
+pthread_t t2;
+pthread_t t3;
+void *serverResponse(void *)
 {
-  int port = 2401;
-  char message[2100];
+  listen(serverSocket, 5);
 
-  sockaddr_in serverAddress;
-  memset(&serverAddress, '\0', sizeof(serverAddress));
-  serverAddress.sin_family = AF_INET;
-  serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-  serverAddress.sin_port = htons(port);
+  clientDescriptor1 = accept(serverSocket, (sockaddr *)&clientAddress, &clientAddressLength);
 
-  int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-  bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
-
-  listen(serverSocket, 1);
-
-  sockaddr_in clientAddress;
-  socklen_t clientAddressLength = sizeof(clientAddress);
-  int clientDescriptor = accept(serverSocket, (sockaddr *)&clientAddress, &clientAddressLength);
   cout << "CONNECTED " << endl;
-
   while (true)
   {
     memset(&message, 0, sizeof(message));
-    recv(clientDescriptor, (char *)&message, sizeof(message), 0);
+    recv(clientDescriptor1, (char *)&message, sizeof(message), 0);
 
     cout << "Client: " << message << endl;
 
     if (strcmp(message, "QUIT") == 0)
     {
       cout << "ENDING SESSION" << endl;
-      send(clientDescriptor, (char *)"QUIT", strlen(message), 0);
-      break;
+      send(clientDescriptor1, (char *)"QUIT", strlen(message), 0);
+      exit(0);
     }
 
-    // string input;
-    // getline(cin, input);
     memset(&message, 0, sizeof(message));
     strcpy(message, "TEST RESPONSE");
-    send(clientDescriptor, (char *)&message, strlen(message), 0);
+    send(clientDescriptor1, (char *)&message, strlen(message), 0);
   }
-  close(clientDescriptor);
+  return 0;
+}
+
+void *serverResponse2(void *)
+{
+  listen(serverSocket, 5);
+
+  clientDescriptor2 = accept(serverSocket, (sockaddr *)&clientAddress, &clientAddressLength);
+
+  cout << "CONNECTED " << endl;
+  while (true)
+  {
+    memset(&message, 0, sizeof(message));
+    recv(clientDescriptor2, (char *)&message, sizeof(message), 0);
+
+    cout << "Client: " << message << endl;
+
+    if (strcmp(message, "QUIT") == 0)
+    {
+      cout << "ENDING SESSION" << endl;
+      send(clientDescriptor2, (char *)"QUIT", strlen(message), 0);
+      exit(0);
+    }
+
+    memset(&message, 0, sizeof(message));
+    strcpy(message, "TEST RESPONSE");
+    send(clientDescriptor2, (char *)&message, strlen(message), 0);
+  }
+  return 0;
+}
+
+int main(int argc, char *argv[])
+{
+  memset(&serverAddress, '\0', sizeof(serverAddress));
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
+  serverAddress.sin_port = htons(port);
+
+  bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+
+  while (true)
+  {
+    pthread_create(&t1, NULL, serverResponse, NULL);
+    pthread_create(&t2, NULL, serverResponse2, NULL);
+  }
+  close(clientDescriptor1);
   close(serverSocket);
 }
